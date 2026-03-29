99ef58aa823b0d3f8ee45438b28f7fe0d76831e0
Ángel Xavier Pons Márquez <angelxavierponsmarquez2018@gmail.com>
2026-03-29 15:27:11 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,8 @@ const port = process.env.PORT || 3000;
 app.use(cors({
     origin: process.env.CORS_ORIGIN || 'http://localhost:4200'
 }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // 📌 Middleware Multitenant (Selección Dinámica de DB)
 app.use((req, res, next) => {
